8e23bfc91948db822a190b1a753747e5aaf85b59
Johannes Engell Kamber <JK-mber@users.noreply.github.com>
2018-01-02 14:24:25 +0100

    Updates to include stock to keep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 
+KEEP_STOCK_TIME_RATIO = 0.25
+
 
 class Material:
     def __init__(self, raw_mat_dict):
@@ -9,6 +11,8 @@ class Material:
         self.value = None
         self.accu_crafting_time = None
         self.batch = 1
+        self.stock_per_acc_c_hrs = 0
+        self.stock_to_keep = 0;
 
         if 'toMake' in raw_mat_dict:
             self.to_make = {}
@@ -25,19 +29,41 @@ class Material:
             self.batch = int(raw_mat_dict['batch'])
 
     def get_accu_crafting_time(self, mat_dict, crafters):
-        accu_crafting_time = self.time / \
-            int(crafters[self.source]['amount']) / self.batch
-        if self.to_make is not None:
-            for mat in self.to_make:
-                # if (print_level): print()
-                # if (print_level): print(accu_crafting_time)
-                accu_crafting_time += \
-                    mat_dict[mat].get_accu_crafting_time(mat_dict, crafters) \
-                    * self.to_make[mat]
-                # if (print_level): print(accu_crafting_time)
+        if self.accu_crafting_time is None:
+            accu_crafting_time = self.time / int(crafters[self.source]['amount'])
+            if self.to_make is not None:
+                for mat in self.to_make:
+                    # if (print_level): print()
+                    # if (print_level): print(accu_crafting_time)
+                    accu_crafting_time += \
+                        mat_dict[mat].get_accu_crafting_time(mat_dict, crafters) \
+                        * self.to_make[mat]
+                    # if (print_level): print(accu_crafting_time)
 
-        self.accu_crafting_time = accu_crafting_time
+            self.accu_crafting_time = accu_crafting_time / self.batch
+            if accu_crafting_time != 0:
+                self.stock_per_acc_c_hrs = 1*60*60/accu_crafting_time
+
         return self.accu_crafting_time
+
+    def update_stock_to_keep(self, mat_dict, crafters, amount=None):
+        if amount is None:
+            amount = self.stock_per_acc_c_hrs
+
+        if self.time/int(crafters[self.source]['amount']) > KEEP_STOCK_TIME_RATIO * self.accu_crafting_time:
+            keep_stock = True
+        else:
+            keep_stock = False
+        # print('amount: ' + str(amount))
+        # print('stock_to_keep: ' + str(self.stock_to_keep))
+        if keep_stock:
+            self.stock_to_keep += amount
+        else:
+            if self.to_make is not None:
+                for mat in self.to_make:
+                    # print('mat: ' + mat)
+                    # print('self.to_make: ' + str(self.to_make))
+                    mat_dict[mat].update_stock_to_keep(mat_dict, crafters, self.to_make[mat] * amount)
 
 
 def load_mats():
@@ -67,6 +93,12 @@ for m in mats:
     mats[m].get_accu_crafting_time(mats, crafters)
     # print(m + ': ' + str(mats[m].get_accu_crafting_time(mats)))
 
+for m in mats:
+    # print('Callling update_stock_to_keep() for mat: ' + m)
+    mats[m].update_stock_to_keep(mats, crafters)
+    # import pdb; pdb.set_trace()
+
+
 mats_sorted = sorted(mats.items(), key=lambda x: x[1].accu_crafting_time)
 for mat in mats_sorted:
     # pass
@@ -74,6 +106,15 @@ for mat in mats_sorted:
         print(mat[0] + ": " + str(60*60*250/mat[1].accu_crafting_time))
 
 
+with open('crafting_hours.csv','w') as f:
+    f.write('Material,Accumulated crafting time,Amount (1 crafting hour),Stock to keep (1 crafting hr),Amount (250 crafting hours),Stock to keep (250 crafting hrs)\n')
+    for m in mats_sorted:
+        try:
+            crafting_hr_amount = 60*60/m[1].accu_crafting_time
+        except ZeroDivisionError:
+            crafting_hr_amount = 0
+
+        f.write(str(m[0]) +','+ str(m[1].accu_crafting_time) +','+ str(crafting_hr_amount)  +','+ str(m[1].stock_to_keep) +','+ str(250*crafting_hr_amount)  +','+ str(m[1].stock_to_keep*250) + '\n')
 
 import pdb; pdb.set_trace()
 # return mats
